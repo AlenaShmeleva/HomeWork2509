f8852af2295512c93969cfc3bb368444b3deb6a1
Alyona <nakamura-san@mail.ru>
2022-09-25 18:06:52 +0300

    Task 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 Console.Clear();
-Console.WriteLine("Ввведите число A: ");
+Console.WriteLine("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ввведите число B: ");
+Console.WriteLine("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 int  result = 1;
 
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while (a > 0)
+{
+    result = result + a % 10;
+    a = a / 10;
+}
+Console.WriteLine();
+Console.WriteLine("Сумма цифр в числе = " + result);
+
